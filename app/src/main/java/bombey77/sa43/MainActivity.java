d1a13bd2fd53ef89788aec1d1609ee2911af5d54
Ромашка <bombey77@mail.ru>
2016-11-03 00:38:39 +0200

    Создание множественного выбора

--- app/src/main/java/bombey77/sa43/MainActivity.java
+++ app/src/main/java/bombey77/sa43/MainActivity.java
@@ -3,6 +3,7 @@ package bombey77.sa43;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -26,15 +27,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         names = getResources().getStringArray(R.array.nameList);
 
-        listView = (ListView) findViewById(R.id.listView);
-        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.nameList, android.R.layout.simple_list_item_multiple_choice);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.nameList, android.R.layout.simple_list_item_single_choice);
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         listView.setAdapter(adapter);
     }
 
     @Override
     public void onClick(View v) {
-        Log.d(LOG, "Result is " + names[listView.getCheckedItemPosition()]);
-    }
+        Log.d(LOG, "Result is: ");
+
+        SparseBooleanArray sba = listView.getCheckedItemPositions();
+        for (int i = 0; i < sba.size(); i++) {
+            int key = sba.keyAt(i);
+            if (sba.get(key))
+                Log.d(LOG, "Check: " + names[key]);
+        }
+     }
 }
